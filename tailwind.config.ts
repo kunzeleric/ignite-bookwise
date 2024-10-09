70c34ec2d74f5cba31c12c96fa435a8b0c1a9aec
Eric Kunzel <kunzel.eric@gmail.com>
2024-10-09 15:58:56 -0300

    chore: configured style, font + removed global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,37 @@
-import type { Config } from "tailwindcss";
+/* eslint-disable @typescript-eslint/no-require-imports */
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    colors: {
+      green: {
+        100: '#50B2C0',
+        200: '#255D6A',
+        300: '#0A313C',
       },
+      purple: {
+        100: '#8381D9',
+        200: '#2A2879',
+      },
+      gray: {
+        100: '#F8F9FC',
+        200: '#E6E8F2',
+        300: '#D1D6E4',
+        400: '#8D95AF',
+        500: '#303F73',
+        600: '#252D4A',
+        700: '#181C2A',
+        800: '#0E1116',
+      },
+      'gradient-vertical': `linear-gradient(180deg, #7FD1CC 0%, #9694F5 100%)`,
+      'gradient-horizontal': `linear-gradient(90deg, #7FD1CC 0%, #9694F5 100%)`,
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require('@tailwindcss/forms')],
+}
+export default config
