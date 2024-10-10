0c1ff08735e890bad689cd19dee554313342ee27
Eric Kunzel <kunzel.eric@gmail.com>
2024-10-10 17:32:31 -0300

    fix: changed color config to extend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,28 +8,31 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      green: {
-        100: '#50B2C0',
-        200: '#255D6A',
-        300: '#0A313C',
+    extend: {
+      colors: {
+        green: {
+          100: '#50B2C0',
+          200: '#255D6A',
+          300: '#0A313C',
+        },
+        purple: {
+          100: '#8381D9',
+          200: '#2A2879',
+        },
+        gray: {
+          100: '#F8F9FC',
+          200: '#E6E8F2',
+          300: '#D1D6E4',
+          400: '#8D95AF',
+          500: '#303F73',
+          600: '#252D4A',
+          700: '#181C2A',
+          800: '#0E1116',
+        },
       },
-      purple: {
-        100: '#8381D9',
-        200: '#2A2879',
+      backgroundImage: {
+        hero: "url('/images/login-hero.png')",
       },
-      gray: {
-        100: '#F8F9FC',
-        200: '#E6E8F2',
-        300: '#D1D6E4',
-        400: '#8D95AF',
-        500: '#303F73',
-        600: '#252D4A',
-        700: '#181C2A',
-        800: '#0E1116',
-      },
-      'gradient-vertical': `linear-gradient(180deg, #7FD1CC 0%, #9694F5 100%)`,
-      'gradient-horizontal': `linear-gradient(90deg, #7FD1CC 0%, #9694F5 100%)`,
     },
   },
   plugins: [require('@tailwindcss/forms')],
