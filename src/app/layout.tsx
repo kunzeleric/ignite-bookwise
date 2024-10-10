5bbbaa8e4bf70c6dd6bbe58655b862f95b4e6f5b
Eric Kunzel <kunzel.eric@gmail.com>
2024-10-10 17:33:05 -0300

    refactor: modified layout structure and created initial login page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${nunito.className} antialiased`}>{children}</body>
+      <body className={`${nunito.className} bg-gray-800 antialiased`}>
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,57 @@
-export default function Home() {
-  return <main>Hello world!</main>
+import Image from 'next/image'
+
+export default function Login() {
+  return (
+    <main className="min-h-screen">
+      <section className="flex h-screen items-center justify-center">
+        <div className="bg-hero mx-4 object-cover bg-no-repeat w-full min-h-[912px] max-w-[598px]" />
+        <div className="flex flex-col gap-10 pl-56">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-white font-semibold text-2xl">Boas vindas!</h1>
+            <p className="text-white font-light">
+              Faça seu login ou acesse como visitante.
+            </p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <div className="bg-gray-600 cursor-pointer min-w-[372px] px-6 py-5 flex items-center gap-4 rounded-md">
+              <Image
+                className="w-8 h-8"
+                src="/images/logo-google.png"
+                alt=""
+                width={50}
+                height={50}
+              />
+              <p className="text-lg text-nowrap font-semibold text-white">
+                Entrar com Google
+              </p>
+            </div>
+            <div className="bg-gray-600 cursor-pointer min-w-[372px] px-6 py-5 flex items-center gap-4 rounded-md">
+              <Image
+                className="w-8 h-8"
+                src="/images/logo-github.png"
+                alt=""
+                width={50}
+                height={50}
+              />
+              <p className="text-lg text-nowrap font-semibold text-white">
+                Entrar com Github
+              </p>
+            </div>
+            <div className="bg-gray-600 cursor-pointer min-w-[372px] px-6 py-5 flex items-center gap-4 rounded-md">
+              <Image
+                className="w-8 h-8"
+                src="/images/logo-rocket.png"
+                alt=""
+                width={50}
+                height={50}
+              />
+              <p className="text-lg text-nowrap font-semibold text-white">
+                Entrar como visitante
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  )
 }
